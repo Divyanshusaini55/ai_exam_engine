8819f2234cd53826e9811cf3c1f3262d32dd6e77
Divyanshusaini55 <22f2000900@ds.study.iitm.ac.in>
2026-09-07 15:11:31 +0530

    urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -21,5 +21,10 @@ urlpatterns = [
     path('', home),
 ]
 
+from django.urls import re_path
+from django.views.static import serve
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
